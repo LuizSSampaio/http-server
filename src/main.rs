ec3db6606e9ae73fd4f1109e24a15fe63130c74f
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2025-11-10 14:08:28 -0300

    refactor: handle request in a separeted function

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,30 @@
-use std::{io::Write, net::TcpListener};
+use std::{
+    io::Write,
+    net::{TcpListener, TcpStream},
+};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(mut stream) => {
+            Ok(stream) => {
                 println!("accepted new connection");
-                let buf = b"HTTP/1.1 200 OK\r\n\r\n";
-                stream.write_all(buf).unwrap();
+                if let Err(e) = handle_client(stream) {
+                    println!("error: {}", e);
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
+    }
+}
+
+fn handle_client(mut stream: TcpStream) -> anyhow::Result<()> {
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    match stream.write_all(response.as_bytes()) {
+        Ok(_) => Ok(()),
+        Err(e) => anyhow::bail!(e),
     }
 }
