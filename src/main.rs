8e7ce646c23df1635347b4de0f9c0c333059aec8
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2025-11-10 15:06:36 -0300

    refactor(http): change hard coded text to http crate Status Code constants

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use std::{
     net::{TcpListener, TcpStream},
 };
 
+use http::StatusCode;
+
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
@@ -26,11 +28,15 @@ fn handle_client(mut stream: TcpStream) -> anyhow::Result<()> {
     let request = reader.lines().next().unwrap().unwrap();
 
     let status = match &request[..] {
-        "GET / HTTP/1.1" => "HTTP/1.1 200 OK",
-        _ => "HTTP/1.1 404 Not Found",
+        "GET / HTTP/1.1" => StatusCode::OK,
+        _ => StatusCode::NOT_FOUND,
     };
 
-    let response = format!("{status}\r\n\r\n");
+    let response = format!(
+        "HTTP/1.1 {} {}\r\n\r\n",
+        status.as_str(),
+        status.canonical_reason().unwrap_or("")
+    );
     match stream.write_all(response.as_bytes()) {
         Ok(_) => Ok(()),
         Err(e) => anyhow::bail!(e),
